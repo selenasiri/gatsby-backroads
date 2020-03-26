ecf94a0a3da737b7f9536968634949e1001f3df0
Selena Siri <selenasiri@gmail.com>
2020-03-25 23:29:26 -0400

    backroads add init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 import React from "react"
+import Layout from "../components/Layout"
 
-export default () => <div>Hello world!</div>
+const index = () => <Layout>Hello world!123</Layout>
+
+export default index
