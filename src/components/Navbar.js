2432c929ea48abff3009f00e7c2dd32208395565
Selena Siri <selenasiri@gmail.com>
2020-03-28 18:41:01 -0400

    add navbar and footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,65 @@
-import React from "react"
+import React, { useState } from "react"
+import { Link } from "gatsby"
+// import { FaRProject } from "react-icons/fa"
+import { FaAlignRight } from "react-icons/fa"
+import links from "../constants/links"
+import socialIcons from "../constants/social-icons"
+import logo from "../images/logo.svg"
+import styles from "../css/navbar.module.css"
 
 const Navbar = () => {
-  return <div>i'm Navbar</div>
+  const [isOpen, setNav] = useState(false)
+
+  const toggleNav = () => {
+    setNav(isOpen => !isOpen)
+  }
+
+  console.log(isOpen)
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navCenter}>
+        <div className={styles.navHeader}>
+          <img src={logo} alt="backroads logo" />
+
+          <button type="button" className={styles.logoBtn} onClick={toggleNav}>
+            <FaAlignRight className={styles.logoIcon} />
+          </button>
+        </div>
+
+        <ul
+          className={
+            isOpen
+              ? `${styles.navLinks} ${styles.showNav}`
+              : `${styles.navlinks}`
+          }
+        >
+          {links.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link to={item.path}>{item.text}</Link>
+              </li>
+            )
+          })}
+        </ul>
+
+        <div className={styles.navSocialLinks}>
+          {socialIcons.map((item, index) => {
+            return (
+              <a
+                key={index}
+                href={item.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {item.icon}
+              </a>
+            )
+          })}
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
---
+++ src/constants/links.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    path: "/",
+    text: "home",
+  },
+  {
+    path: "/tours",
+    text: "tours",
+  },
+  {
+    path: "/blog",
+    text: "blog",
+  },
+  {
+    path: "/contact",
+    text: "contact",
+  },
+]
+
+export default links
