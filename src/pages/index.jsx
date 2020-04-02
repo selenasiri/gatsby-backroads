46c8c06553bde397c2e76fba3e9c457167273f08
Selena Siri <selenasiri@gmail.com>
2020-04-02 18:04:34 -0400

    error page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,21 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
 
 const index = () => (
   <Layout>
-    <h1>hi from home</h1>
+    <SimpleHero>
+      <Banner
+        title="continue exploring"
+        info=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, aut?"
+      >
+        <Link to="/tours" className="btn-white">
+          explore tours
+        </Link>
+      </Banner>
+    </SimpleHero>
   </Layout>
 )
 
