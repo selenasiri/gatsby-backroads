c146767a442b1d2d31de2281f2ea5d955b947c79
Selena Siri <selenasiri@gmail.com>
2020-04-14 21:01:18 -0400

    gatsby-image

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import Title from '../Title'
 import styles from '../../css/about.module.css'
-import img from '../../images/defaultBcg.jpeg'
+// import img from '../../images/defaultBcg.jpeg'
+import {useStaticQuery, graphql} from 'gatsby'
+import Img from "gatsby-image"
+
+const getAbout = graphql`
+{
+  aboutImage: file(relativePath: {eq: "defaultBcg.jpeg"}) {
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`
 
 const About = () => {
+const {aboutImage} = useStaticQuery(getAbout)
+console.log("YYYYY", aboutImage)
+
   return (
     <section className={styles.about}>
       hello from about
@@ -11,7 +28,8 @@ const About = () => {
       <div className={styles.aboutCenter}>
         <article className={styles.aboutImg}>
           <div className={styles.imgContainer}>
-            <img src={img} alt="about company" />
+            {/* <img src={img} alt="about company" /> */}
+            <Img fluid={aboutImage.childImageSharp.fluid} alt="awesome"/>
           </div>
         </article>
         <article className={styles.aboutInfo}>
